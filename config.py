156d4cb735180c5548d24bc681439a55ba154060
GSvegetable <3301296046@qq.com>
2026-07-24 19:12:44 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+# 你的主控机器人 Token
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8821795395:AAEdQZljpRy4KPn0KXTG2xCJrOoB06L_so4")
+
+# 数据库连接串（直接从 Railway Variables 里读）
+DATABASE_URL = os.getenv("DATABASE_URL")
